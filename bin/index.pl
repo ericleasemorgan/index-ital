348e9f4eeffed7565d57083b5eb9ea74e0b52724
Eric Lease Morgan <emorgan@nd.edu>
2020-03-15 20:26:23 -0400

    Enhanced indexing

--- bin/index.pl
+++ bin/index.pl
@@ -37,12 +37,17 @@ while( my $bibliographics = $handle->fetchrow_hashref ) {
 	# parse the easy stuff
 	my $bid        = $$bibliographics{ 'bid' };
 	my $identifier = $$bibliographics{ 'identifier' };
+	my $source     = $$bibliographics{ 'source' };
+	my $publisher  = $$bibliographics{ 'publisher' };
+	my $language   = $$bibliographics{ 'language' };
 	my $title      = $$bibliographics{ 'title' };
 	my $abstract   = $$bibliographics{ 'abstract' };
 	my $doi        = $$bibliographics{ 'doi' };
 	my $words      = $$bibliographics{ 'words' };
 	my $sentences  = $$bibliographics{ 'sentences' };
 	my $flesch     = $$bibliographics{ 'flesch' };
+	my $date       = $$bibliographics{ 'date' };
+	my $url        = $$bibliographics{ 'url' };
 
 	if ( $words     < 1 )     { $words = 1 }
 	if ( $sentences < 1 ) { $sentences = 1 }
@@ -64,18 +69,30 @@ while( my $bibliographics = $handle->fetchrow_hashref ) {
 	$subhandle->execute() or die $DBI::errstr;
 	while( my @author = $subhandle->fetchrow_array ) { push @authors, $author[ 0 ] }
 	
+	# get entities
+	my @entities       = ();
+	my $subhandle = $dbh->prepare( qq(SELECT DISTINCT( entity ) FROM entities WHERE bid='$bid' AND ( type IS 'PERSON' OR type IS 'ORG' ) ORDER BY entity;) );
+	$subhandle->execute() or die $DBI::errstr;
+	while( my @entity = $subhandle->fetchrow_array ) { push @entities, $entity[ 0 ] }
+	
 	# debug; dump
 	warn "         bid: $bid\n";
 	warn "  identifier: $identifier\n";
+	warn "        date: $date\n";
+	warn "      source: $source\n";
+	warn "   publisher: $publisher\n";
+	warn "    language: $language\n";
 	warn "        file: $file\n";
 	warn "       title: $title\n";
 	warn "    abstract: $abstract\n";
 	warn "         DOI: $doi\n";
 	warn "       words: $words\n";
+	warn "         url: $url\n";
 	warn "   sentences: $sentences\n";
 	warn "      flesch: $flesch\n";
 	warn "  keyword(s): ", join( '; ', @keywords ), "\n";
 	warn "   author(s): ", join( '; ', @authors ), "\n";
+	warn "  entitie(s): ", join( '; ', @entities ), "\n";
 	warn "\n";
 	
 	my $fulltext = &slurp( $file );
@@ -84,9 +101,26 @@ while( my $bibliographics = $handle->fetchrow_hashref ) {
 	$fulltext    =~ s/ +/ /g;
 	$fulltext    =~ s/[^\x09\x0A\x0D\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]//go;
 	
+	# escape entities; should be clean up further back the chain, hmmmm
+	my @normalized_entities = ();
+	foreach my $entity ( @entities ) {
+	
+		$entity    =~ s/\r//g;
+		$entity    =~ s/\n/ /g;
+		$entity    =~ s/ +/ /g;
+		$entity    =~ s/[^\x09\x0A\x0D\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]//go;
+		push ( @normalized_entities, $entity );
+		
+	}
+	@entities = @normalized_entities;
+		
 	# create data
 	my $solr_bid        = WebService::Solr::Field->new( 'bid'        => $bid );
 	my $solr_identifier = WebService::Solr::Field->new( 'identifier' => $identifier );
+	my $solr_date       = WebService::Solr::Field->new( 'date'       => $date );
+	my $solr_source     = WebService::Solr::Field->new( 'source'     => $source );
+	my $solr_publisher  = WebService::Solr::Field->new( 'publisher'  => $publisher );
+	my $solr_language   = WebService::Solr::Field->new( 'language'   => $language );
 	my $solr_fulltext   = WebService::Solr::Field->new( 'fulltext'   => $fulltext );
 	my $solr_title      = WebService::Solr::Field->new( 'title'      => $title );
 	my $solr_abstract   = WebService::Solr::Field->new( 'abstract'   => $abstract );
@@ -94,14 +128,19 @@ while( my $bibliographics = $handle->fetchrow_hashref ) {
 	my $solr_words      = WebService::Solr::Field->new( 'words'      => $words );
 	my $solr_sentences  = WebService::Solr::Field->new( 'sentences'  => $sentences );
 	my $solr_flesch     = WebService::Solr::Field->new( 'flesch'     => $flesch );
+	my $solr_url        = WebService::Solr::Field->new( 'url'        => $url );
 
 	# fill a solr document with simple fields
 	my $doc = WebService::Solr::Document->new;
-	$doc->add_fields( $solr_bid, $solr_identifier, $solr_fulltext, $solr_title, $solr_abstract, $solr_doi, $solr_words, $solr_sentences, $solr_flesch );
+	$doc->add_fields( $solr_bid, $solr_identifier, $solr_fulltext, $solr_title, $solr_abstract, $solr_doi, $solr_words, $solr_sentences, $solr_flesch, $solr_date, $solr_language, $solr_publisher, $solr_source, $solr_url );
 
 	# add complex fields
-	foreach ( @keywords ) { $doc->add_fields( ( WebService::Solr::Field->new( 'keywords' => $_ ) ) ) }
-	foreach ( @authors )  { $doc->add_fields( ( WebService::Solr::Field->new( 'authors' => $_ ) ) ) }
+	foreach ( @keywords ) { $doc->add_fields( ( WebService::Solr::Field->new( 'keywords'       => $_ ) ) ) }
+	foreach ( @keywords ) { $doc->add_fields( ( WebService::Solr::Field->new( 'facet_keywords' => $_ ) ) ) }
+	foreach ( @authors )  { $doc->add_fields( ( WebService::Solr::Field->new( 'authors'        => $_ ) ) ) }
+	foreach ( @authors )  { $doc->add_fields( ( WebService::Solr::Field->new( 'facet_authors'  => $_ ) ) ) }
+	foreach ( @entities ) { $doc->add_fields( ( WebService::Solr::Field->new( 'entities'       => $_ ) ) ) }
+	foreach ( @entities ) { $doc->add_fields( ( WebService::Solr::Field->new( 'facet_entities' => $_ ) ) ) }
 
 	# save/index
 	$solr->add( $doc );
--- bin/search.pl
+++ bin/search.pl
@@ -3,13 +3,13 @@
 # search.pl - command-line interface to search a solr instance
 
 # Eric Lease Morgan <emorgan@nd.edu>
-# April 30, 2019 - first cut; based on earlier work
-# May    2, 2019 - added classification and files (urls)
-# July 8, 2019 - added author, title, and date
+# (c) University of Notre Dame; distributed under GNU Public License
+
+# March 15, 2010 - first cut/documentation; based on bunches o' other implementations
 
 
 # configure
-use constant FACETFIELD => ( );
+use constant FACETFIELD => ( facet_keywords, facet_authors, facet_entities );
 use constant SOLR       => 'http://localhost:8983/solr/ital';
 use constant TXT        => './tmp/txt';
 use constant PDF        => './tmp/pdf';
@@ -39,8 +39,26 @@ my $total = $response->content->{ 'response' }->{ 'numFound' };
 # get number of hits returned
 my @hits = $response->docs;
 
+# build a list of keyword facets
+my @facet_keywords = ();
+my $keyword_facets = &get_facets( $response->facet_counts->{ facet_fields }->{ facet_keywords } );
+foreach my $facet ( sort { $$keyword_facets{ $b } <=> $$keyword_facets{ $a } } keys %$keyword_facets ) { push @facet_keywords, $facet . ' (' . $$keyword_facets{ $facet } . ')'; }
+
+# build a list of author facets
+my @facet_authors = ();
+my $author_facets = &get_facets( $response->facet_counts->{ facet_fields }->{ facet_authors } );
+foreach my $facet ( sort { $$author_facets{ $b } <=> $$author_facets{ $a } } keys %$author_facets ) { push @facet_authors, $facet . ' (' . $$author_facets{ $facet } . ')'; }
+
+# build a list of entity facets
+my @facet_entities = ();
+my $entity_facets = &get_facets( $response->facet_counts->{ facet_fields }->{ facet_entities } );
+foreach my $facet ( sort { $$entity_facets{ $b } <=> $$entity_facets{ $a } } keys %$entity_facets ) { push @facet_entities, $facet . ' (' . $$entity_facets{ $facet } . ')'; }
+
 # start the output
 print "Your search found $total item(s) and " . scalar( @hits ) . " item(s) are displayed.\n\n";
+print '  keyword facets: ', join( '; ', @facet_keywords ), "\n\n";
+print '   author facets: ', join( '; ', @facet_authors ), "\n\n";
+print '   entity facets: ', join( '; ', @facet_entities ), "\n\n";
 
 # loop through each document
 for my $doc ( $response->docs ) {
@@ -52,14 +70,20 @@ for my $doc ( $response->docs ) {
 	# parse
 	my $bid        = $doc->value_for(  'bid' );
 	my $identifier = $doc->value_for(  'identifier' );
+	my $date       = $doc->value_for(  'date' );
+	my $source     = $doc->value_for(  'source' );
+	my $publisher  = $doc->value_for(  'publisher' );
+	my $langauge   = $doc->value_for(  'language' );
 	my $title      = $doc->value_for(  'title' );
 	my $abstract   = $doc->value_for(  'abstract' );
 	my $doi        = $doc->value_for(  'doi' );
 	my $words      = $doc->value_for(  'words' );
 	my $sentences  = $doc->value_for(  'sentences' );
 	my $flesch     = $doc->value_for(  'flesch' );
+	my $url        = $doc->value_for(  'url' );
 	my @keywords   = $doc->values_for( 'keywords' );
 	my @authors    = $doc->values_for( 'authors' );
+	my @entities   = $doc->values_for( 'entities' );
 
 	# create file names
 	my $file = substr( $identifier, rindex( $identifier, '/' ) + 1, length( $identifier ) );
@@ -67,26 +91,30 @@ for my $doc ( $response->docs ) {
 	$pdf    = "$pdf/$file.pdf";
 	
 	# output
-	print "   author(s): ", join( '; ', @authors ), "\n";
-	print "       title: $title\n";
-	print "    abstract: $abstract\n";
-	print "  keyword(s): ", join( '; ', @keywords ), "\n";
-	print "          id: $bid\n";
-	print "         DOI: $doi\n";
-	print "         OAI: $identifier\n";
-	print "  plain text: $txt\n";
-	print "         PDF: $pdf\n";
-	print "       words: $words\n";
-	print "   sentences: $sentences\n";
-	print "      flesch: $flesch\n";
+	print "       author(s): ", join( '; ', @authors ), "\n";
+	print "           title: $title\n";
+	print "            date: $date\n";
+	print "          source: $source\n";
+	print "       publisher: $publisher\n";
+	print "        langauge: $langauge\n";
+	print "        abstract: $abstract\n";
+	print "      keyword(s): ", join( '; ', @keywords ), "\n";
+	print "     entities(s): ", join( '; ', @entities ), "\n";
+	print "             bid: $bid\n";
+	print "             DOI: $doi\n";
+	print "             OAI: $identifier\n";
+	print "      plain text: $txt\n";
+	print "  PDF (cononial): $url\n";
+	print "    PDF (cached): $pdf\n";
+	print "           words: $words\n";
+	print "       sentences: $sentences\n";
+	print "          flesch: $flesch\n";
 	print "\n";
 
 }
 
 # done
 exit;
-
-
 
 
 # convert an array reference into a hash
